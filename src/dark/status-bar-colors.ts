8ce5d1cd2e719591f6349e1f365f946673587f7b
Magnesium- <yyong007@outlook.com>
2022-11-15 17:38:45 +0800

    feat: adv status bar

--- src/dark/status-bar-colors.ts
+++ src/dark/status-bar-colors.ts
@@ -3,18 +3,18 @@ import type { StatusBar, StatusBarItem } from '../types/StatusBar'
 import colors, { mapColorsWithRuntimeWeight } from '../designs/index'
 
 const statusBarColors = (): Partial<StatusBar> => ({
-  [`statusBar.foreground`]: colors.grayColors100,
-  [`statusBar.background`]: mapColorsWithRuntimeWeight('900')
+  [`statusBar.foreground`]: mapColorsWithRuntimeWeight('500'),
+  [`statusBar.background`]: colors.grayColors900
 })
 
 const statusBarItemColors = (): Partial<StatusBarItem> => ({
   [`statusBarItem.activeBackground`]: mapColorsWithRuntimeWeight('1000'),
-  [`statusBarItem.hoverBackground`]: colors.grayColors1000,
+  [`statusBarItem.hoverBackground`]: colors.grayColors100,
   [`statusBarItem.prominentForeground`]: mapColorsWithRuntimeWeight('500'),
-  [`statusBarItem.prominentBackground`]: colors.grayColors100,
-  [`statusBarItem.prominentHoverBackground`]: colors.grayColors100,
-  [`statusBarItem.remoteBackground`]: mapColorsWithRuntimeWeight('700'),
-  [`statusBarItem.remoteForeground`]: colors.grayColors100,
+  [`statusBarItem.prominentBackground`]: colors.grayColors1200,
+  [`statusBarItem.prominentHoverBackground`]: colors.grayColors1100,
+  [`statusBarItem.remoteBackground`]: colors.grayColors1000,
+  [`statusBarItem.remoteForeground`]: mapColorsWithRuntimeWeight('500'),
   [`statusBarItem.errorBackground`]: colors.redColors700,
   [`statusBarItem.errorForeground`]: colors.grayColors100,
   [`statusBarItem.warningBackground`]: colors.yellowColors700,
