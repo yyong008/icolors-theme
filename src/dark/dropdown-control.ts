2cbed59a75c93261e0c074af80d400b1b1fdfbbf
Magnesium- <yyong007@outlook.com>
2022-11-15 17:37:54 +0800

    feat: adv dropdown

--- src/dark/dropdown-control.ts
+++ src/dark/dropdown-control.ts
@@ -4,7 +4,6 @@ import type { Dropdown } from '../types/Dropdown'
 
 const dropdown = (): Partial<Dropdown> => ({
   ['dropdown.listBackground']: colors.grayColors1000,
-  // ['dropdown.listForeground']: colors.grayColors100,
   ['dropdown.background']: colors.grayColors1200,
   ['dropdown.border']: mapColorsWithRuntimeWeight('400'),
   ['dropdown.foreground']: colors.grayColors300
